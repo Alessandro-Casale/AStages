6036a7fdf42d7aedd5cae138dc3eaf53429d8616
Alessandro-Casale <alessandro.casale.developer@gmail.com>
2026-04-07 09:15:58 +0200

    api: Hide AHolder dev message under ENABLE_DEV_LOGS common config flag, change flag default value to false

--- src/main/java/com/alessandro/astages/api/holder/AHolder.java
+++ src/main/java/com/alessandro/astages/api/holder/AHolder.java
@@ -7,6 +7,7 @@ import com.alessandro.astages.api.nullability.NotNullParamsAndMethodsReturn;
 import com.alessandro.astages.api.nullability.Nullable;
 import com.alessandro.astages.infrastructure.capability.OfflinePlayerStage;
 import com.alessandro.astages.infrastructure.capability.ServerStage;
+import com.alessandro.astages.infrastructure.config.AStagesCommon;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.entity.player.Player;
 import net.neoforged.neoforge.server.ServerLifecycleHooks;
@@ -59,7 +60,10 @@ public class AHolder {
             holder.addPlayer(player);
         } else {
             holder.setPlayer(false);
-            AStages.LOGGER.debug("Encountered null player, skipped adding it to holder!");
+
+            if (AStagesCommon.ENABLE_DEV_LOGS.get()) {
+                AStages.LOGGER.debug("Encountered null player, skipped adding it to holder!");
+            }
         }
 
         return holder;

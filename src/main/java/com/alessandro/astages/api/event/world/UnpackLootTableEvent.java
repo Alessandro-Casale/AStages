c4cb1c86b666288db488b59c520774900d825720
alessandro <alessandro.casale.developer@gmail.com>
2026-08-04 22:30:55 +0200

    api: Mark update events as not yet implemented and deprecate UnpackLootTableEvent

--- src/main/java/com/alessandro/astages/api/event/world/UnpackLootTableEvent.java
+++ src/main/java/com/alessandro/astages/api/event/world/UnpackLootTableEvent.java
@@ -2,4 +2,5 @@ package com.alessandro.astages.api.event.world;
 
 import net.neoforged.bus.api.Event;
 
+@Deprecated(forRemoval = true, since = "3.0.0")
 public class UnpackLootTableEvent extends Event { }
